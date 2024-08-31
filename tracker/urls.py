d3078ea2653cd56d3a9a572d54f4f4c6eff5bc67
TanmaySubhedar <tanmaysubhedar03@gmail.com>
2024-08-31 16:43:38 +0530

    added chart functionality

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('chart/<str:coin_id>/', views.chart, name='chart'),
 ]
+
--- tracker/views.py
+++ tracker/views.py
@@ -11,4 +11,23 @@ def home(request):
         'sparkline': 'false'
     })
     crypto_data = response.json()
+    print(crypto_data)
     return render(request, "tracker/home.html", {'crypto_data': crypto_data})
+
+def chart(request, coin_id):
+    # Fetch historical data for the selected coin
+    response = requests.get(f'https://api.coingecko.com/api/v3/coins/{coin_id}/market_chart', params={
+        'vs_currency': 'usd',
+        'days': '7'  # Fetch data for the last 7 days
+    })
+    chart_data = response.json()
+
+    # Extract timestamps and prices
+    timestamps = [data_point[0] for data_point in chart_data['prices']]
+    prices = [data_point[1] for data_point in chart_data['prices']]
+
+    return render(request, 'tracker/chart.html', {
+        'timestamps': timestamps,
+        'prices': prices,
+        'coin_id': coin_id
+    })
